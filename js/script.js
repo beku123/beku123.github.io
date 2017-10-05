83a7f52b724a8946355a609ead52a6ac681ce4ab
Bartosz Parzychowski <parzychowskibartosz@gmail.com>
2017-10-05 23:56:28 +0200

    newposition

--- js/script.js
+++ js/script.js
@@ -1,2 +1,46 @@
 'use strict';
+$(document).ready(function(){
 
+    
+  scrollMenu();
+    smoothScroll();
+    
+});
+
+function smoothScroll() {
+    
+    $('#main-nav a[href*="#"]:not([href="#"])').click(function(){
+      $('body').animate({scrollTop: $(this.hash).offset().top - 50},500);
+});
+}
+    
+function scrollMenu(){
+    var menuHeight = $('#main-nav').height();
+    var scrolled = $(window).scrollTop();
+    
+    $(window).scroll(function() {
+        var scrolled = $(window).scrollTop();
+        if(scrolled >= menuHeight) {
+            $('#main-nav').addClass('scrolled');
+        
+        }else{
+            $('#main-nav').removeClass('scrolled');
+        }
+    });
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
